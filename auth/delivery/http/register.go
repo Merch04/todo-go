f6f3a4b97194a8a74118b10a86373be7cfd70350
Merch04 <kirill004mel@mail.ru>
2024-09-19 02:12:22 +0700

    create auth module

--- auth/delivery/http/register.go
+++ auth/delivery/http/register.go
@@ -1,1 +1,16 @@
 package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"todo/auth"
+)
+
+func RegisterHTTPEndpoints(router *gin.Engine, uc auth.UseCase) {
+	h := NewHandler(uc)
+
+	authEndpoints := router.Group("/auth")
+	{
+		authEndpoints.POST("/sign-up", h.SignUp)
+		authEndpoints.POST("/sign-in", h.SignIn)
+	}
+}
