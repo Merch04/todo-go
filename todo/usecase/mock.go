e90c1e6449c189c61f5ba89b479bad981f8dd1bd
Merch04 <kirill004mel@mail.ru>
2024-09-21 18:45:12 +0700

    adding blank identifier

--- todo/usecase/mock.go
+++ todo/usecase/mock.go
@@ -10,23 +10,23 @@ type TodoUseCaseMock struct {
 	mock.Mock
 }
 
-func (m *TodoUseCaseMock) Create(ctx context.Context, title, description string, user *models.User) error {
+func (m *TodoUseCaseMock) Create(_ context.Context, title, description string, user *models.User) error {
 	args := m.Called(title, description, user)
 
 	return args.Error(0)
 }
-func (m *TodoUseCaseMock) Delete(ctx context.Context, id string, user *models.User) error {
+func (m *TodoUseCaseMock) Delete(_ context.Context, id string, user *models.User) error {
 	args := m.Called(id, user)
 
 	return args.Error(0)
 }
-func (m *TodoUseCaseMock) Get(ctx context.Context, user *models.User) ([]*models.Task, error) {
+func (m *TodoUseCaseMock) Get(_ context.Context, user *models.User) ([]*models.Task, error) {
 	args := m.Called(user)
 
 	return args.Get(0).([]*models.Task), args.Error(1)
 }
 
-func (m *TodoUseCaseMock) ChangeStatus(ctx context.Context, id string, status bool, user *models.User) error {
+func (m *TodoUseCaseMock) ChangeStatus(_ context.Context, id string, status bool, user *models.User) error {
 	args := m.Called(id, status, user)
 
 	return args.Error(0)
